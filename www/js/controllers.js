c3d5683ac73755819b8432beb2c0d0bc0bc492bb
Kristjan Ulst <kristjan.ulst@ekspressdigital.eu>
2015-04-01 11:43:46 +0300

    uuendus

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('controllers', ['highcharts-ng'])
+angular.module('controllers', ['highcharts-ng', 'ionic.contrib.ui.cards'])
 
     // Menu button controller
     .controller('menuButton', function ($scope, $ionicSideMenuDelegate) {
@@ -45,3 +45,59 @@ angular.module('controllers', ['highcharts-ng'])
         }
 
     })
+
+    .directive('noScroll', function($document) {
+
+        return {
+            restrict: 'A',
+            link: function($scope, $element, $attr) {
+
+                $document.on('touchmove', function(e) {
+                    e.preventDefault();
+                });
+            }
+        }
+    })
+
+
+    .controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate) {
+        var cardTypes = [{
+            title: 'Swipe down to clear the card',
+            image: 'img/pic.png'
+        }, {
+            title: 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.',
+            image: 'img/pic.png'
+        }, {
+            title: 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.',
+            image: 'img/pic2.png'
+        }, {
+            title: 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.',
+            image: 'img/pic3.png'
+        }, {
+            title: 'What kind of clouds are these?',
+            image: 'img/pic4.png'
+        }];
+
+        $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
+
+        $scope.cardSwiped = function(index) {
+            $scope.addCard();
+        };
+
+        $scope.cardDestroyed = function(index) {
+            $scope.cards.splice(index, 1);
+        };
+
+        $scope.addCard = function() {
+            var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+            newCard.id = Math.random();
+            $scope.cards.push(angular.extend({}, newCard));
+        }
+    })
+
+    .controller('CardCtrl', function($scope, $ionicSwipeCardDelegate) {
+        $scope.goAway = function() {
+            var card = $ionicSwipeCardDelegate.getSwipeableCard($scope);
+            card.swipe();
+        };
+    });
